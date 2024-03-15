6fdb2b6e325b0bf42a7849a80bab6612becfa620
Mike_Curley <michaelcurley1@gmail.com>
2024-03-15 04:58:37 +0000

    Adding required UCI dataset and adding header to dataset

--- analysis.py
+++ analysis.py
@@ -9,7 +9,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load the iris dataset
-df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+#The dataset contained no headers so I added them in the names parameter. Reference: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.read_csv.html
+df = pd.read_csv('iris.data', header=None, names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species'])
+
 
 # View the first 10 rows
 print(df.head(10))
+
+# Print a sumamary of the dataset
+print(df.describe())
