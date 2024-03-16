5f46d5ae0a48ebb245ef1ba9880516399cdb6eb4
Michael Curley <michaelcurley1@gmail.com>
2024-03-16 08:33:10 +0000

    Adding some plots and analysis to analysis.py

--- analysis.py
+++ analysis.py
@@ -18,3 +18,35 @@ print(df.head(10))
 
 # Print a sumamary of the dataset
 print(df.describe())
+
+#Checking missing values
+print(df.isnull().sum())
+
+#Checking for duplicates. df.drop_duplicates() will remove all duplicates from the dataset
+data = df.drop_duplicates(subset='species',)
+print(data)
+
+#Checking the group size of each species
+print(df.groupby('species').size())
+
+#plotting the dataset
+setosa = df[df.species == "Iris-setosa"]
+versicolor = df[df.species=='Iris-versicolor']
+virginica = df[df.species=='Iris-virginica']
+
+fig, ax = plt.subplots()
+fig.set_size_inches(13, 7) # adjusting the length and width of plot
+
+# lables and scatter points
+ax.scatter(setosa['petal_length'], setosa['petal_width'], label="Setosa", facecolor="blue")
+ax.scatter(versicolor['petal_length'], versicolor['petal_width'], label="Versicolor", facecolor="green")
+ax.scatter(virginica['petal_length'], virginica['petal_width'], label="Virginica", facecolor="red")
+
+
+ax.set_xlabel("petal length (cm)")
+ax.set_ylabel("petal width (cm)")
+ax.grid()
+ax.set_title("Iris petals")
+ax.legend()
+
+plt.show()
