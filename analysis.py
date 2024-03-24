a8674bba352091c374cde62c8094a051bd92d33e
Mike_Curley <michaelcurley1@gmail.com>
2024-03-24 19:55:18 +0000

    Updating analysis.py for variable summaries

--- analysis.py
+++ analysis.py
@@ -51,6 +51,54 @@ def create_summary(df, FILENAME): #The function takes in two parameters, the dat
 
         file.write("****************************************************************************************************\n")
 
+        # Write a summary of the Sepal Length variable to the file
+        file.write('\n')
+        file.write("Summary of the Sepal Length Variable of the Dataset\n\n")
+        #The describe() function applies basic statistical computations on the dataset like extreme values, 
+        #count of data points standard deviation, etc. Any missing value or NaN value is automatically skipped. 
+        #describe() function gives a good picture of the distribution of data
+        #https://www.geeksforgeeks.org/exploratory-data-analysis-on-iris-dataset/
+        sepal_length_summary = df[["sepal_length", "species"]].groupby("species").describe()
+        file.write(sepal_length_summary.to_string() + "\n\n")
+        file.write("\n")
+        file.write("****************************************************************************************************\n")
+
+        # Write a summary of the Sepal Width variable to the file
+        file.write('\n')
+        file.write("Summary of the Sepal Width Variable of the Dataset\n\n")
+        #The describe() function applies basic statistical computations on the dataset like extreme values, 
+        #count of data points standard deviation, etc. Any missing value or NaN value is automatically skipped. 
+        #describe() function gives a good picture of the distribution of data
+        #https://www.geeksforgeeks.org/exploratory-data-analysis-on-iris-dataset/
+        sepal_width_summary = df[["sepal_width", "species"]].groupby("species").describe()
+        file.write(sepal_width_summary.to_string() + "\n\n")
+        file.write("\n")
+        file.write("****************************************************************************************************\n")
+
+        # Write a summary of the Petal Length variable to the file
+        file.write('\n')
+        file.write("Summary of the Petal Length Variable of the Dataset\n\n")
+        #The describe() function applies basic statistical computations on the dataset like extreme values, 
+        #count of data points standard deviation, etc. Any missing value or NaN value is automatically skipped. 
+        #describe() function gives a good picture of the distribution of data
+        #https://www.geeksforgeeks.org/exploratory-data-analysis-on-iris-dataset/
+        petal_length_summary = df[["petal_length", "species"]].groupby("species").describe()
+        file.write(petal_length_summary.to_string() + "\n\n")
+        file.write("\n")
+        file.write("****************************************************************************************************\n")
+
+        # Write a summary of the Petal Width to the file
+        file.write('\n')
+        file.write("Summary of the Petal Width Variable of the Dataset\n\n")
+        #The describe() function applies basic statistical computations on the dataset like extreme values, 
+        #count of data points standard deviation, etc. Any missing value or NaN value is automatically skipped. 
+        #describe() function gives a good picture of the distribution of data
+        #https://www.geeksforgeeks.org/exploratory-data-analysis-on-iris-dataset/
+        petal_width_summary = df[["petal_width", "species"]].groupby("species").describe()
+        file.write(petal_width_summary.to_string() + "\n\n")
+        file.write("\n")
+        file.write("****************************************************************************************************\n")
+
         #Write the number of rows and columns in the dataset to the file
         file.write('\n')
         file.write("Number of rows and columns in the dataset\n\n")
